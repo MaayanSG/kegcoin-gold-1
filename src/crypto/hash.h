ee1193aed7b243bea80f008a3adb02df9bb8299a
Leo Cornelius <48571127+LeoCornelius@users.noreply.github.com>
2019-03-28 18:08:57 +0000

    Add cn keg

--- src/crypto/hash.h
+++ src/crypto/hash.h
@@ -26,10 +26,10 @@
 #define CN_DARK_SCRATCHPAD              524288
 #define CN_DARK_ITERATIONS              262144
 
-// Standard CryptoNight Turtle
-#define CN_TURTLE_PAGE_SIZE             262144
-#define CN_TURTLE_SCRATCHPAD            262144
-#define CN_TURTLE_ITERATIONS            131072
+// Standard CryptoNight keg
+#define CN_TURTLE_PAGE_SIZE             131072
+#define CN_TURTLE_SCRATCHPAD            131072
+#define CN_TURTLE_ITERATIONS            65536
 
 // CryptoNight Soft Shell Definitions
 #define CN_SOFT_SHELL_MEMORY            262144 // This defines the lowest memory utilization for our curve
